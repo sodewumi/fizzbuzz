29381f8e21477315fac08ac66b71eafc15dc38e6
distor <stellaodewumi@gmail.com>
2014-12-03 00:50:18 -0600

    pushed fizzbuzz to an array

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 $(document).ready(function() {
 
 	//check function checks if the value entered into the input field is a number
-
 	function check() {
 		var number_check = document.getElementById('field_num').value;
 		if (isNaN(number_check)) {
@@ -14,28 +13,50 @@ $(document).ready(function() {
 	}
 
 	//fizzBuzz function grabs the number as well, and implements fizzbuzz
-
 	function fizzBuzz() {
 		var number_fb = document.getElementById('field_num').value; 
+		//var change = document.getElementById('text').innerHTML
 
 		if (check()) {
+			//removes show div, and reveals hide div
+			$('#show').fadeOut(1000);
+			$('#hide').show();
+
+			//create a new paragraph underneath the hide div w/ id of "addText"
+			var div = document.getElementById('hide');
+			var	p = document.createElement('p');
+			p.setAttribute('id', 'addText')
+			div.appendChild(p);
+
+
+			//create variables and array for the for loop
+			var arr = [];
+			var fzbz = "fizz buzz";
+			var fz = "fizz";
+			var bz = "buzz";
+
+
+			// for loop pushes, fizz, buzz, fizzbuzz, or i into the array
 			for(var i=1; i <= number_fb; i++) {
 				if (i % 3 === 0 && i % 5 === 0) {
-					console.log('fizz buzz');
+					p.appendChild('yo');
+					arr.push(fzbz);
 				} else if(i % 3 === 0) {
-					console.log('fizz');
+					arr.push(fz);
 				} else if (i % 5 === 0) {
-					console.log('buzz');
+					arr.push(bz);
 				} else {
-					console.log(i);
+					arr.push(i);
 				}
 			}
+
+			// p.appendChild('yo');
 		} else {
 			console.log('please enter a real number');
 		}
 	}
 
 
-	$('button').on('click', fizzBuzz)
+	$('button').on('click', fizzBuzz);
 	
 })
